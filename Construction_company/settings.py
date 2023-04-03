3001c063dfcb33b1ab354e3a63b91b2dae1b46be
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-04-03 20:34:39 +0300

    added orender to the ALLOWED_HOSTS

--- Construction_company/settings.py
+++ Construction_company/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "construction-company.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
