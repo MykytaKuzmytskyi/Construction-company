a586e4fc9da3561ae36e17a6fbb09d21402c11af
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-04-03 20:28:20 +0300

    Deleted .local from wsgi

--- Construction_company/settings.py
+++ Construction_company/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
